086824db44a9570e45fb6863a67c4b5bf28075db
hohyuncheon <75802657+hohyuncheon@users.noreply.github.com>
2021-03-08 12:43:39 +0900

    p

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function showclick(){
+    var context = document.getElementById("contacttext")
+    context.style.visibility = "visible"
+}
+
+function li(listindex){
+
+    var hoverr = document.querySelectorAll(.navbar);
+}
